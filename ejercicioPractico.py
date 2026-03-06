d882e1fc2170aba8431d330119814a567a0c9af8
VanessaGY <vanessa.ynz.b3ll@gmail.com>
2026-03-06 10:59:45 -0500

    Refactor Empleado class and add salary methods

--- ejercicioPractico.py
+++ ejercicioPractico.py
@@ -1,15 +1,27 @@
 class Empleado:
-    def __init__(self, nombre, cargo, salario_mensual):
-        self.nombre = input("Ingrese su nombre:")
-        self.cargo = input("Ingrese su cargo:")
-        self.salario_mensual = float(input("Ingrese su salario mensual:"))
-        
-class Empresa:
-    def __init__(self, nombre):
-        self.nombre = nombre
-    def empresa_empleado(self, empleado):
-        print(f"El empleado {empleado.nombre} trabaja en {self.nombre}.")
+    empresa = "TechSolutions Inc."
 
-if __name__ == "__main__":
-    empleado1 = Empleado("","",0)
-    print(f"Empleado: {empleado1.nombre}, Cargo: {empleado1.cargo}, Salario Mensual: ${empleado1.salario_mensual:.2f}")
+    def __init__(self, nombre, cargo, salario_mensual):
+        self.nombre = nombre
+        self.cargo = cargo
+        self.salario_mensual = salario_mensual
+    
+    def describir(self):
+        print(f"{self.nombre} es {self.cargo} y gana {self.salario_mensual}.")
+
+    def calcular_anual(self):
+        return f"{self.salario_mensual*12}"
+    
+    def aumentar_sueldo(self, porcentaje):
+        aumento = self.salario_mensual*(porcentaje/100)
+        self.salario_mensual += aumento
+        print(f"Nuevo salario: {self.salario_mensual}")
+
+empleado1= Empleado("Ana", "Coordinadora", 2000)
+empleado1.describir()
+
+salario_anual = empleado1.calcular_anual()
+print(f"Su salario anual es de: ${salario_anual}")
+
+porcentaje = int(input("Ingrese el porcentaje de aumento: "))
+amuntar_sueldo = empleado1.aumentar_sueldo(porcentaje)
