7438a2eaab835ffb5032c7bb67f82438d4d89ccb
vanniesven <guerreroyanezv@gmail.com>
2026-03-20 11:09:48 -0500

    cambio de cuenta al correo guerreroyanezv@gmail

---
+++ SEMANA 3/clase 2003/python.py
@@ -0,0 +1,14 @@
+class Libro:
+    def __init__(self, codigo, titulo, autor, precio_alquiler):
+        self.codigo = codigo
+        self.titulo = titulo
+        self.autor = autor
+        self.precio_alquiler = precio_alquiler
+        
+
+class Usuarios:
+    def __init__(self, id, nombre, nivel_membresia):
+        self.id = id
+        self.nombre = nombre
+        self.nivel_membresia = nivel_membresia
+        pass
